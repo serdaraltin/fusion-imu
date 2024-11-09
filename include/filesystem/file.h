170c0295fcd5959eec97ad225dae34ec4f385663
serdaraltin <seyupaltin@gmail.com>
2024-11-09 23:26:56 +0300

    refactor project structure

---
+++ include/filesystem/file.h
@@ -0,0 +1,14 @@
+//
+// Created by Serdar on 9.11.2024.
+//
+
+#ifndef FUSION_SENS_FILE_H
+#define FUSION_SENS_FILE_H
+
+
+class File {
+
+};
+
+
+#endif //FUSION_SENS_FILE_H
---
+++ src/filesystem/file.cpp
@@ -0,0 +1,5 @@
+//
+// Created by main on 9.11.2024.
+//
+
+#include "file.h"
