4195e393cea3a2dc038772d5ff462fad2e5dac1f
serdaraltin <seyupaltin@gmail.com>
2024-11-19 21:28:55 +0300

    update run_tests.sh

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Test sonuçları için dizini oluştur (eğer yoksa)
 mkdir -p test/report
 
-# JSON dosyasının adını ve yolunu oluştur
 json_filename="test/report/$(basename $(pwd))-$(date +'%Y%m%d%H%M%S').json"
 
-# Testi çalıştır ve JSON çıktısını belirtilen dosyaya kaydet
 pio test -e esp32-Test --json-output-path="${json_filename}.temp"
 
-# JSON dosyasını formatla ve aynı dosya adıyla kaydet
 cat "${json_filename}.temp" | jq . > $json_filename
 
 rm "${json_filename}.temp"
