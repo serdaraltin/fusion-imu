2e1fb3fb38ca496650591a0a51ec9cc7b1173437
serdaraltin <seyupaltin@gmail.com>
2025-01-26 14:49:56 +0300

    refactor(board switch): switched between esp8266 and esp32

---
+++ include/comms/wifi.h
@@ -0,0 +1,14 @@
+//
+// Created by Serdar on 11.12.2024.
+//
+
+#ifndef FUSION_SENS_WIFI_H
+#define FUSION_SENS_WIFI_H
+
+
+class wifi {
+
+};
+
+
+#endif //FUSION_SENS_WIFI_H
---
+++ src/comms/wifi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Serdar on 11.12.2024.
+//
+
+#include "comms/wifi.h"
